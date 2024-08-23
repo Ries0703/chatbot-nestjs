fc6177afbcd0118b910c37a09eb93a45dce42666
Ries0703 <117291686+Ries0703@users.noreply.github.com>
2024-08-23 15:48:54 +0700

    import QueueModule to both AppModule and WorkerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { WebhookModule } from './webhook/webhook.module';
 import { QueueModule } from './queue/queue.module';
-import { RepositoryModule } from './repository/repository.module';
 
 @Module({
-  imports: [WebhookModule, QueueModule, RepositoryModule],
+  imports: [WebhookModule, QueueModule],
 })
 export class AppModule {}
--- src/worker.module.ts
+++ src/worker.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { OpenaiModule } from './openai/openai.module';
 import { FacebookModule } from './facebook/facebook.module';
 import { BackgroundModule } from './background/background.module';
+import { RepositoryModule } from './repository/repository.module';
+import { QueueModule } from './queue/queue.module';
 
 @Module({
-  imports: [OpenaiModule, FacebookModule, BackgroundModule],
+  imports: [
+    QueueModule,
+    OpenaiModule,
+    FacebookModule,
+    BackgroundModule,
+    RepositoryModule,
+  ],
 })
 export class WorkerModule {}
